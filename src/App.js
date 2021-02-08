30e867fc9be985acc7c0fb45ee1e6d76c0a494c4
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-02-08 09:50:31 +0200

    valmis palautus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+function App () {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [result, setResult] = useState(0);
+
+  function calculate (e) {
+    e.preventDefault() 
+    let litres = bottles * 0.33;
+    let grams = litres * 8 * 4.5;
+    let burning = weight / 10;
+    let gramsLeft = grams - burning * time;
+    let result = 0;
+    if (gender === 'male') {
+      result = gramsLeft / (weight * 0.7);
+    } else if (gender === 'female') {
+      result = gramsLeft / (weight * 0.6);
+    }
+    
+    setResult(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculating alcohol blood level</h1>
+      <form onSubmit={calculate}>
+        <div className='rivi'>
+          <label>Weight </label>
+          <input name='weight' type='number' step='1' value={weight} onChange={e => setWeight(e.target.value)} ></input>
+        </div>
+        <div className='rivi'>
+          <label>Bottles </label>
+          <input name='bottles' type='number' value={bottles} onChange={e => setBottles(e.target.value)} ></input>
+        </div>
+        <div className='rivi'>
+          <label>Time </label>
+          <input name='time' type='number' value={time} onChange={e => setTime(e.target.value)} ></input>
+        </div>
+        <div className='rivi'>
+          <label>Gender </label>
+          <input type='radio' name='gender' value='male' defaultChecked onChange={e => setGender(e.target.value)} />
+          <label>Male</label>
+          <input type='radio' name='gender' value='female' onChange={e => setGender(e.target.value)} />
+          <label>Female</label>
+        </div>
+        <div className='rivi'>
+          <output>{result}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
